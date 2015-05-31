f3bc7d609308551f79944f0cbf574466a0ba18f7
muxin <muxin4ever@gmail.com>
2015-05-30 20:41:58 -0700

    add showcase info

---
+++ showcase_info.py
@@ -0,0 +1,285 @@
+# Github Showcase Infomation as of 2015-06-01
+
+# showcase of front end javascript frameworks: 12 repos
+sc_frontend_javascript_frameworks = [
+    'angular/angular.js', 
+    'jashkenas/backbone', 
+    'emberjs/ember.js', 
+    'knockout/knockout',
+    'tastejs/todomvc',
+    'spine/spine',
+    'Polymer/polymer',
+    'mozbrick/brick',
+    'facebook/react',
+    'dojo/dojo',
+    'finom/matreshka',
+    'aurelia/framework'
+]
+
+# showcase of data visulization: 22 repos
+sc_data_visualization = [
+    'mbostock/d3',
+    'benpickles/peity',
+    'gka/chroma.js',
+    'okfn/recline',
+    'jacomyal/sigma.js',
+    'samizdatco/arbor',
+    'HumbleSoftware/envisionjs',
+    'kartograph/kartograph.js',
+    'trifacta/vega',
+    'stamen/modestmaps-js',
+    'Leaflet/Leaflet',
+    'matplotlib/matplotlib',
+    'Kozea/pygal',
+    'dc-js/dc.js',
+    'nnnick/Chart.js',
+    'fastly/epoch',
+    'AnalyticalGraphicsInc/cesium',
+    'densitydesign/raw',
+    'gionkunz/chartist-js',
+    'ecomfe/echarts',
+    'mozilla/metrics-graphics',
+    'PMSI-AlignAlytics/dimple'
+]
+
+# showcase of web application frameworks: 18 repos
+sc_web_application_frameworks = [
+    'django/django',
+    'rails/rails',
+    'cakephp/cakephp',
+    'perl-catalyst/catalyst-runtime',
+    'ninjaframework/ninja',
+    'revel/revel',
+    'strongloop/express',
+    'sinatra/sinatra',
+    'mitsuhiko/flask',
+    'meteor/meteor',
+    'playframework/playframework',
+    'symfony/symfony',
+    'laravel/laravel',
+    'kraih/mojo',
+    'balderdashy/sails',
+    'lotus/lotus',
+    'pakyow/pakyow',
+    'web2py/web2py'
+]
+
+# showcase of machine learning: 20 repos
+sc_machine_learning = [
+    'nikolaypavlov/MLPNeuralNet',
+    'karpathy/convnetjs',
+    'ryanb/ruby-warrior',
+    'PredictionIO/PredictionIO',
+    'scikit-learn/scikit-learn',
+    'numenta/nupic',
+    'johnmyleswhite/ML_for_Hackers',
+    'h2oai/h2o-2',
+    'danielsdeleo/Decider',
+    'harthur/brain',
+    'apache/mahout',
+    'sjwhitworth/golearn',
+    'cloudera/oryx',
+    'mikeizbicki/HLearn',
+    'JohnLangford/vowpal_wabbit',
+    'shogun-toolbox/shogun',
+    'BVLC/caffe',
+    'SeldonIO/seldon-server',
+    'jubatus/jubatus',
+    'clips/pattern'
+]
+
+# showcase of nosql databases: 13 repos
+sc_nosql_databases = [
+    'basho/riak',
+    'mongodb/mongo',
+    'antirez/redis',
+    'rethinkdb/rethinkdb',
+    'pouchdb/pouchdb',
+    'twitter/flockdb',
+    'ravendb/ravendb',
+    'thinkaurelius/titan',
+    'jankotek/mapdb',
+    'neo4j/neo4j',
+    'orientechnologies/orientdb',
+    'apache/couchdb',
+    'arangodb/arangodb'
+]
+
+# showcase of game engines: 12 repos
+sc_game_engines = [
+    'okamstudio/godot',
+    'turbulenz/turbulenz_engine',
+    'GarageGames/Torque3D',
+    'GarageGames/Torque2D',
+    'spring/spring',
+    'cocos2d/cocos2d-x',
+    'Gamua/Starling-Framework',
+    'AdamsLair/duality',
+    'OpenRA/OpenRA',
+    'gameplay3d/GamePlay',
+    'jMonkeyEngine/jmonkeyengine',
+    'Circular-Studios/Dash'
+]
+
+# showcase of javascript game engines: 12 repos
+sc_javascript_game_engines = [
+    'GoodBoyDigital/pixi.js',
+    'photonstorm/phaser',
+    'melonjs/melonJS',
+    'gamelab/kiwi.js',
+    'craftyjs/Crafty',
+    'wellcaffeinated/PhysicsJS',
+    'piqnt/stage.js',
+    'cocos2d/cocos2d-html5',
+    'playcanvas/engine',
+    'LazerUnicorns/lycheeJS',
+    'BabylonJS/Babylon.js',
+    'ekelokorpi/panda-engine'
+]
+
+# showcase of 3d modeling: 10 repos
+sc_3d_modeling = [
+    'canadaduane/house',
+    'carlosgs/Cyclone-PCB-Factory',
+    'glennswest/kosselg1',
+    'underverk/3D_Printer',
+    'skalnik/secret-bear-clip',
+    'skalnik/peg-board-spindle',
+    'reprap/huxley',
+    'gklyne/things',
+    'eweinhoffer/Bike-Accessories',
+    'iliterobotics/3D'
+]
+
+# showcase of design essentials: 17 repos
+sc_design_essentials = [
+    'alexwolfe/Buttons',
+    'twbs/bootstrap',
+    'daneden/animate.css',
+    'nathansmith/960-Grid-System',
+    'necolas/normalize.css',
+    'joshuaclayton/blueprint-css',
+    'subtlepatterns/SubtlePatterns',
+    'mrmrs/colors',
+    'driftyco/ionicons',
+    'designmodo/Flat-UI',
+    'h5bp/html5-boilerplate',
+    'Compass/compass',
+    'zurb/foundation',
+    'Modernizr/Modernizr',
+    'twbs/ratchet',
+    'flexiblegs/flexiblegs',
+    'IanLunn/Hover'
+]
+
+# showcase of css preprocessors: 7 repos
+sc_css_preprocessors = [
+    'sass/sass',
+    'less/less.js',
+    'stylus/stylus',
+    'livereload/LiveReload',
+    'Subash/Prepros',
+    'sass/libsass',
+    'idiot/Spiffing'
+]
+
+# showcase of fonts: 12 repos
+sc_fonts = [
+    'google/roboto',
+    'adobe-fonts/source-serif-pro',
+    'adobe-fonts/source-code-pro',
+    'theleagueof/league-gothic',
+    'adobe-fonts/source-sans-pro',
+    'theleagueof/raleway',
+    'theleagueof/chunk',
+    'theleagueof/junction',
+    'theleagueof/ostrich-sans',
+    'klepas/open-baskerville',
+    'mozilla/Fira',
+    'google/fonts'
+]
+
+# showcase of icon fonts: 15 repos
+sc_icon_fonts = [
+    'FortAwesome/Font-Awesome',
+    'driftyco/ionicons',
+    'somerandomdude/Iconic',
+    'MfgLabs/mfglabs-iconset',
+    'parkerbennett/stackicons',
+    'fontello/brandico.font',
+    'pfefferle/openwebicons',
+    'stephenhutchings/typicons.font',
+    'samcollins/css-social-buttons',
+    'Keyamoon/IcoMoon-Free',
+    'scottdejonge/Map-Icons',
+    'jhogue/PE-Analog-Clock-icon-font',
+    'erikflowers/weather-icons',
+    'whhglyphs/webhostinghub-glyphs',
+    'github/octicons'
+]
+
+# showcase of package managers: 22 repos
+sc_package_managers = [
+    'rubygems/rubygems',
+    'jordansissel/fpm',
+    'pypa/pip',
+    'Homebrew/homebrew',
+    'bower/bower',
+    'npm/npm',
+    'CocoaPods/CocoaPods',
+    'componentjs/component',
+    'volojs/volo',
+    'caolan/jam',
+    'composer/composer',
+    'supermarin/Alcatraz',
+    'ocaml/opam',
+    'wbond/package_control',
+    'atom/apm',
+    'gmarik/Vundle.vim',
+    'chocolatey/chocolatey',
+    'rust-lang/cargo',
+    'gpmgo/gopm',
+    'Carthage/Carthage',
+    'freshshell/fresh',
+    'spmjs/spm'
+]
+
+# showcase of programming languages: 23 repos
+sc_programming_languages = [
+    'ruby/ruby',
+    'D-Programming-Language/dmd',
+    'JuliaLang/julia',
+    'rust-lang/rust',
+    'clojure/clojure',
+    'scala/scala',
+    'elixir-lang/elixir',
+    'timburks/nu',
+    'stevedekorte/io',
+    'gkz/LiveScript',
+    'elm-lang/elm-compiler',
+    'eholk/harlan',
+    'php/php-src',
+    'jashkenas/coffeescript',
+    'Araq/Nim',
+    'fsharp/fsharp',
+    'JetBrains/kotlin',
+    'amber-smalltalk/amber',
+    'plt/racket',
+    'erlang/otp',
+    'golang/go',
+    'groovy/groovy-core',
+    'dylan-lang/opendylan'
+]
+
+# showcase of open source operating systems: 8 repos
+sc_open_source_operating_systems = [
+    'KnightOS/KnightOS',
+    'klange/toaruos',
+    'ReturnInfinity/BareMetal-OS',
+    'SamyPesse/How-to-Make-a-Computer-Operating-System',
+    'cloudius-systems/osv',
+    'andreasgal/B2G',
+    'charliesome/jsos',
+    'torvalds/linux'
+]
+
